3b2c4da80777635393904aff673732a16fef60c7
SudevOP1 <168339977+SudevOP1@users.noreply.github.com>
2024-04-29 18:25:55 +0530

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -5,7 +5,8 @@ function appendToDisplay(input){
         display.value += input;
     }
     if(display.value.includes("Error")){
-        display.value="Error";
+        clearDisplay();
+        appendToDisplay(input);
     }
 }
 
@@ -13,26 +14,42 @@ function clearDisplay(){
     display.value = "";
 }
 
-function calculate(){
-    if(eval(display.value.length) < 10) {
-        try{
-            display.value = eval(display.value);
-        }
-        catch(error){
+function calculate() {
+    if (display.value.length < 10) {
+        try {
+            if (display.value.includes("^")) {
+                const values = display.value.split("^");
+                if (values.length === 2) {
+                    const base = parseFloat(values[0]);
+                    const exponent = parseFloat(values[1]);
+                    display.value = power(base, exponent);
+                } 
+                else {
+                    display.value = "Error";
+                }
+            } else {
+                display.value = eval(display.value);
+            }
+        } catch (error) {
             display.value = "Error";
         }
-    }
-    else {
-        display.value="Error"
-    }
-    if(display.value.includes("Error")){
-        display.value="Error";
+    } else {
+        display.value = "Error";
     }
 }
+
 
 function backSpace(){
     display.value.length = display.value.length - 1;
     if(display.value("Error")){
         display.value="Error";
     }
+}
+
+function power(x,n){
+    let y = 1;
+    for (let i=1; i<=n; i++) {
+        y = y*x;
+    }
+    return y;
 }
